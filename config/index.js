65ac57315d72e1c20374a3c6d89f050346739e6a
jinxinghong <1096227957@qq.com>
2018-10-11 15:41:50 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 3009,
+    port: 2009,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
